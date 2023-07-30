426518bb736407eefe283af2ce40553cbe249212
yasmmin <ycfrenchgirl2@gmail.com>
2023-07-30 16:30:39 -0300

    starter

---
+++ run_all.sh
@@ -0,0 +1,4 @@
+python3 prediction_s1.py config_carb.json 1
+python3 curation_s2.py config_carb.json 1
+python3 curation_s2.py config_carb.json 2
+python3 antigenicity_prediction_s3.py config_carb.json 1
